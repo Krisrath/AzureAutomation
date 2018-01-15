18913771385a40f28a6b155a2c7bb22fd599a5bc
Krisrath <jeneldar@gmail.com>
2018-01-15 15:53:59 -0500

    Update

--- VMStartStop/StartAllVMsinSub.ps1
+++ VMStartStop/StartAllVMsinSub.ps1
@@ -1,18 +1,18 @@
-Select-AzureSubscription -SubscriptionId 420fd085-3e46-4d03-afb0-dda04c916b0b
-#Write-Host $subscription.SubscriptionName
-
-foreach ($vm in Get-AzureVM)
-{
-    $name = $vm.Name
-    $servicename = $vm.ServiceName
-
-    If($vm.Status -ne 'Running')
+Select-AzureRMSubscription -SubscriptionId 420fd085-3e46-4d03-afb0-dda04c916b0b
+    #Write-Host $subscription.SubscriptionName
+ 
+    foreach ($vm in Get-AzureRmVM)
     {
-        # Add the VM's which should not be shutdown 
-        
-        Write-Host 'Starting ' + $name
-        Start-AzureVM -Service $servicename -name $name
-        
+        $name = $vm.Name
+        $RGName = $vm.ResourceGroupName
+    
+        If($vm.Status -ne 'Running')
+        {
+            # Add the VM's which should not be shutdown 
+            
+            Write-Host 'Starting ' + $name
+            Start-AzureRmVM -ResourceGroupName $RGName -name $name
+            
+        }
+        # do lots of other stuff
     }
-    # do lots of other stuff
-}
