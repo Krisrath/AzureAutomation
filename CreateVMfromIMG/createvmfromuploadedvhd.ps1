b619c5e306c138e6ec3547aa49a46371a0c18cbb
Krisrath <jeneldar@gmail.com>
2018-01-29 09:21:34 -0500

    1
    1

--- CreateVMfromIMG/createvmfromuploadedvhd.ps1
+++ CreateVMfromIMG/createvmfromuploadedvhd.ps1
@@ -2,53 +2,28 @@
 #Get-AzureRmSubscription -SubscriptionId xxxxxxxxxxxxxxxxxxxxxxxxxxxxx | Select-AzureRmSubscription
 Select-AzureRmSubscription -SubscriptionId xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 ## Customize for Your Environment
-$ResourceGroupName = "ManageIQTest"
-$StorageAccountName = "horizonimagestore"
-$BlobNameSource = "manageiq-azure-fine-4fixed.vhd"
-$BlobNameDest = "manageiqosdsk0.vhd"
-$BlobDestinationContainer = "images"
-$VMName = "manageiq-test"
-$DeploySize= "Standard_D2_V3"
-$vmUserName = "manageiqadmin"
-$Location = 'eastus2'
+#Prepare the VM parameters 
+$rgName = "<resource-group-name>"
+$location = "EastUS2"
+$vnet = "<virtual-network>"
+$subnet = "/subscriptions/xxxxxxxxx/resourceGroups/<resource-group-name>/providers/Microsoft.Network/virtualNetworks/<virtual-network>/subnets/<subnet>"
+$nicName = "VM01-Nic-01"
+$vmName = "VM01"
+$osDiskName = "VM01-OSDisk"
+$osDiskUri = "https://<storage-account>.blob.core.windows.net/<container>/server.vhd"
+$VMSize = "Standard_A1"
+$storageAccountType = "StandardLRS"
+$IPaddress = "10.10.10.10"
 
-$InterfaceName = "manageIQ-nic"
-$VNet = Get-AzureRmVirtualNetwork -Name adVNET -ResourceGroupName HorizonTest
-$Subnet = Get-AzureRmVirtualNetworkSubnetConfig -Name adSubnet -VirtualNetwork $VNet
-$PublicIPName = "manageiq-test-public-ip"
+#Create the VM resources
+$IPconfig = New-AzureRmNetworkInterfaceIpConfig -Name "IPConfig1" -PrivateIpAddressVersion IPv4 -PrivateIpAddress $IPaddress -SubnetId $subnet
+$nic = New-AzureRmNetworkInterface -Name $nicName -ResourceGroupName $rgName -Location $location -IpConfiguration $IPconfig
+$vmConfig = New-AzureRmVMConfig -VMName $vmName -VMSize $VMSize
+$vm = Add-AzureRmVMNetworkInterface -VM $vmConfig -Id $nic.Id
 
-$SSHKey = 
+$osDisk = New-AzureRmDisk -DiskName $osDiskName -Disk (New-AzureRmDiskConfig -AccountType $storageAccountType -Location $location -CreateOption Import -SourceUri $osDiskUri) -ResourceGroupName $rgName
+$vm = Set-AzureRmVMOSDisk -VM $vm -ManagedDiskId $osDisk.Id -StorageAccountType $storageAccountType -DiskSizeInGB 128 -CreateOption Attach -Windows
+$vm = Set-AzureRmVMBootDiagnostics -VM $vm -disable
 
-##
-
-$StorageAccount = Get-AzureRmStorageAccount -ResourceGroup 'HorizonTest' -Name $StorageAccountName
-
-$SourceImageUri = "https://$StorageAccountName.blob.core.windows.net/images/$BlobNameSource"
-$Location = $StorageAccount.Location
-$OSDiskName = $VMName
-
-# Network
-$PIp = New-AzureRmPublicIpAddress -Name $PublicIPName -ResourceGroupName $ResourceGroupName -Location $Location -AllocationMethod Dynamic -Force
-#$Subnet = 'adSubnet'
-#$Interface = New-AzureRmNetworkInterface -Name $InterfaceName -ResourceGroupName $ResourceGroupName -Location $Location -Subnet $Subnet -PublicIpAddressId $PIp.Id -Force
-$Interface = New-AzureRmNetworkInterface -Name $InterfaceName -ResourceGroupName $ResourceGroupName -Location $Location -SubnetId $Subnet.Id -PublicIpAddressId $PIp.Id -Force
-# Specify the VM Name and Size
-$VirtualMachine = New-AzureRmVMConfig -VMName $VMName -VMSize $DeploySize
-
-# Add User
-$cred = Get-Credential -UserName $VMUserName -Message "Setting user credential - use blank password"
-$VirtualMachine = Set-AzureRmVMOperatingSystem -VM $VirtualMachine -Linux -ComputerName $VMName -Credential $cred
-
-# Add NIC
-$VirtualMachine = Add-AzureRmVMNetworkInterface -VM $VirtualMachine -Id $Interface.Id
-
-# Add Disk
-$OSDiskUri = $StorageAccount.PrimaryEndpoints.Blob.ToString() + $BlobDestinationContainer + "/" + $BlobNameDest
-
-$VirtualMachine = Set-AzureRmVMOSDisk -VM $VirtualMachine -DiskSizeInGB '127' -Name $OSDiskName -VhdUri $OSDiskUri -CreateOption fromImage -SourceImageUri $SourceImageUri -Linux
-
-# Set SSH key
-Add-AzureRmVMSshPublicKey -VM $VirtualMachine -Path “/home/$VMUserName/.ssh/authorized_keys” -KeyData $SSHKey
-
-# Create the VM
-New-AzureRmVM -ResourceGroupName $ResourceGroupName -Location $Location -VM $VirtualMachine
+#Create the new VM
+New-AzureRmVM -ResourceGroupName $rgName -Location $location -VM $vm
